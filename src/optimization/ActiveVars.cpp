9f2d4122abab3267b5b8317fcf035076018b89a0
totoro <953834564@qq.com>
2021-01-03 14:51:58 +0800

    lab5: update activevars

--- src/optimization/ActiveVars.cpp
+++ src/optimization/ActiveVars.cpp
@@ -18,44 +18,45 @@ void ActiveVars::run()
             func_ = func;
             live_in.clear();
             live_out.clear();
+            int flag = 1;
 
-            for (auto &BB : func_->get_basic_blocks())
+            while (flag == 1)
             {
-                std::set<Value *> InSet = {}, OutSet = {};
-
-                for (auto &succBB : BB->get_succ_basic_blocks())
+                flag = 0;
+                for (auto &BB : func_->get_basic_blocks())
                 {
-                    std::set_union(live_in[succBB].begin(), live_in[succBB].end(), OutSet.begin(), OutSet.end(), std::back_inserter(OutSet));
-                }
-                live_out.insert({BB, OutSet});
+                    std::set<Value *> InSet = {}, OutSet = {};
 
-                /* remove
-                for (auto &itemIn : func_->get_args())
-                {
-                    InSet.insert(itemIn);
-                    // xxx loop
-                }
-                */
-                for (auto &itemUse : func_->get_use_list())
-                {
-                    InSet.insert(itemUse.val_);
-                }
+                    for (auto &succBB : BB->get_succ_basic_blocks())
+                    {
+                        std::set_union(live_in[succBB].begin(), live_in[succBB].end(), OutSet.begin(), OutSet.end(), std::back_inserter(OutSet));
+                    }
+                    live_out.insert({BB, OutSet});
 
-                // get def -> defSet
-                std::set<Value *> tmpSet = {};
-                for (auto &item : OutSet)
-                {
-                    /* TODO
-                    if (item != def)
-                        tmpSet.insert(item);
-                    */
+                    for (auto &itemUse : func_->get_use_list())
+                    {
+                        InSet.insert(itemUse.val_);
+                    }
+
+                    // get def -> defSet(Howto) TODO
+                    std::set<Value *> tmpSet = {};
+                    for (auto &item : OutSet)
+                    {
+                        /*
+                        if (item != def)
+                            tmpSet.insert(item);
+                        */
+                    }
+                    std::set_union(InSet.begin(), InSet.end(), tmpSet.begin(), tmpSet.end(), std::back_inserter(InSet));
+
+                    // if tmpSet or InSet == InSet do nothing
+                    // else flag = 1
+
+                    live_in.insert({BB, InSet});
+
+                    // OutSet_B := all_S_is_B's_successor(InSet_S)
+                    // InSet_B := use_BB + all(OutSet_B - def_BB)
                 }
-                std::set_union(InSet.begin(), InSet.end(), tmpSet.begin(), tmpSet.end(), std::back_inserter(InSet));
-
-                // OutSet_B := all_S_is_B's_successor(InSet_S)
-                // InSet_B := use_BB + all(OutSet_B - def_BB)
-
-                live_in.insert({BB, InSet});
             }
 
             // 在此分析 func_ 的每个bb块的活跃变量，并存储在 live_in live_out 结构内
