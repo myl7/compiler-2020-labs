f89b1a6055e1324786f9ce4adabe1426c69a1fe5
totoro <953834564@qq.com>
2021-01-04 22:15:03 +0800

    lab5: init loopinvhoist for dump graph

--- src/optimization/LoopInvHoist.cpp
+++ src/optimization/LoopInvHoist.cpp
@@ -6,8 +6,25 @@
 void LoopInvHoist::run()
 {
     // 先通过LoopSearch获取循环的相关信息
-    LoopSearch loop_searcher(m_, false);
+    // LoopSearch loop_searcher(m_, false);
+    LoopSearch loop_searcher(m_, true);
     loop_searcher.run();
 
     // 接下来由你来补充啦！
+    /*
+    for (auto &func : m_->get_functions())
+    {
+        for (auto &BB : func->get_basic_blocks())
+        {
+            if (func->get_basic_blocks().empty())
+            {
+                // ENTRY
+                continue;
+            }
+            else
+            {
+            }
+        }
+    }
+    */
 }
