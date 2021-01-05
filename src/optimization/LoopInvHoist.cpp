45e057715395e0e3a1261104892b69cbbd1fa3c8
totoro <953834564@qq.com>
2021-01-05 18:01:36 +0800

    lab5: add to loopinvhoist

--- src/optimization/LoopInvHoist.cpp
+++ src/optimization/LoopInvHoist.cpp
@@ -11,20 +11,31 @@ void LoopInvHoist::run()
     loop_searcher.run();
 
     // 接下来由你来补充啦！
-    /*
+    m_->set_print_name();
     for (auto &func : m_->get_functions())
     {
         for (auto &BB : func->get_basic_blocks())
         {
-            if (func->get_basic_blocks().empty())
+            std::set<Instruction *> left_op_set = {}, right_op_set = {};
+            for (auto &instr : BB->get_instructions())
             {
-                // ENTRY
-                continue;
+                Instruction::OpID type = instr->get_instr_type();
+                // ret br phi gep call alloca
+                if (type == 0 || type == 1 || type == 10 || type == 15 || type == 16 || type == 17)
+                {
+                    continue;
+                }
+                left_op_set.insert(instr);
+                for (auto op : instr->get_operands()) // ? insert instr->get_oprand(0)
+                {
+                    right_op_set.insert(static_cast<Instruction *>(op));
+                }
             }
-            else
+            std::cout << "right_op_set" << '\n';
+            for (auto r : right_op_set)
             {
+                std::cout << r->get_name() << '\n';
             }
         }
     }
-    */
 }
