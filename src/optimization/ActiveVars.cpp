377d73898bfbf1f43bb8946d393c6f50012c2eff
totoro <953834564@qq.com>
2021-01-06 23:11:31 +0800

    lab5: actvar add get args

--- src/optimization/ActiveVars.cpp
+++ src/optimization/ActiveVars.cpp
@@ -1,5 +1,6 @@
 #include "ActiveVars.hpp"
 #include <algorithm>
+#include "logging.hpp"
 
 void ActiveVars::run()
 {
@@ -26,25 +27,22 @@ void ActiveVars::run()
                 for (auto &BB : func_->get_basic_blocks())
                 {
                     std::set<Value *> InSet = {}, OutSet = {};
-
                     for (auto &succBB : BB->get_succ_basic_blocks())
                     {
                         std::set_union(live_in[succBB].begin(), live_in[succBB].end(), OutSet.begin(), OutSet.end(), std::inserter(OutSet, OutSet.begin()));
                     }
                     live_out.insert({BB, OutSet});
-
                     for (auto &itemUse : func_->get_use_list())
                     {
+                        // std::cout << itemUse.val_->get_name();
                         InSet.insert(itemUse.val_);
                     }
-
-                    // get def -> defSet(Howto) TODO
                     std::set<Value *> tmpSet = {}, InSetNew = {};
-                    std::set<Instruction *> defSet = {};
+                    std::set<Value *> defSet = {};
                     for (auto &itemDef : BB->get_instructions())
                     {
                         Instruction::OpID type = itemDef->get_instr_type();
-                        // ret br phi gep call alloca
+                        // skip ret br phi gep call alloca instr
                         if (type == 0 || type == 1 || type == 10 || type == 15 || type == 16 || type == 17)
                         {
                             continue;
@@ -60,26 +58,26 @@ void ActiveVars::run()
                         {
                             defSet.insert(itemDef);
                         }
-                        // if (Value(itemDef))
+                    }
+                    if (BB->get_name() == "label_entry")
+                    {
+                        for (auto &itemDef : func_->get_args())
+                        {
+                            defSet.insert(static_cast<Value *>(itemDef));
+                        }
                     }
 
-                    /*
-                    for (auto &item : OutSet)
-                    {
-                        if (item not in def)
-                            tmpSet.insert(item);
-                    }
-                    */
                     std::set_difference(OutSet.begin(), OutSet.end(), defSet.begin(), defSet.end(), std::inserter(tmpSet, tmpSet.begin()));
                     std::set_union(InSet.begin(), InSet.end(), tmpSet.begin(), tmpSet.end(), std::inserter(InSetNew, InSetNew.begin()));
-
-                    if (!(!(InSet < InSetNew) && !(InSet > InSetNew)))
-                    {
-                        flag = 1;
-                    }
-
+                    // InSetNew will be larger or equal
                     // if ((tmpSet or InSet) == InSet) do nothing
                     // else flag = 1
+                    if (InSet.size() != InSetNew.size())
+                    {
+                        std::cout << BB->get_name() << std::endl;
+                        flag = 1;
+                    }
+                    // std::cout << BB->get_name() << std::endl;
 
                     live_in.insert({BB, InSet});
 
@@ -89,9 +87,6 @@ void ActiveVars::run()
             }
 
             func_->set_instr_name();
-            live_in.clear();
-            live_out.clear();
-
             // 在此分析 func_ 的每个bb块的活跃变量，并存储在 live_in live_out 结构内
 
             output_active_vars << print();
