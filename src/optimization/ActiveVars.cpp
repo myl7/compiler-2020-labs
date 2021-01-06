47eb2e8a52d58a8cdf27c217d2bda662291b9e45
totoro <953834564@qq.com>
2021-01-06 22:04:41 +0800

    lab5: activevars remove unavail fun back_inserter

--- src/optimization/ActiveVars.cpp
+++ src/optimization/ActiveVars.cpp
@@ -29,7 +29,7 @@ void ActiveVars::run()
 
                     for (auto &succBB : BB->get_succ_basic_blocks())
                     {
-                        std::set_union(live_in[succBB].begin(), live_in[succBB].end(), OutSet.begin(), OutSet.end(), std::back_inserter(OutSet));
+                        std::set_union(live_in[succBB].begin(), live_in[succBB].end(), OutSet.begin(), OutSet.end(), std::inserter(OutSet, OutSet.begin()));
                     }
                     live_out.insert({BB, OutSet});
 
@@ -70,8 +70,8 @@ void ActiveVars::run()
                             tmpSet.insert(item);
                     }
                     */
-                    std::set_difference(OutSet.begin(), OutSet.end(), defSet.begin(), defSet.end(), std::back_inserter(tmpSet));
-                    std::set_union(InSet.begin(), InSet.end(), tmpSet.begin(), tmpSet.end(), std::back_inserter(InSetNew));
+                    std::set_difference(OutSet.begin(), OutSet.end(), defSet.begin(), defSet.end(), std::inserter(tmpSet, tmpSet.begin()));
+                    std::set_union(InSet.begin(), InSet.end(), tmpSet.begin(), tmpSet.end(), std::inserter(InSetNew, InSetNew.begin()));
 
                     if (!(!(InSet < InSetNew) && !(InSet > InSetNew)))
                     {
@@ -106,43 +106,53 @@ void ActiveVars::run()
 std::string ActiveVars::print()
 {
     std::string active_vars;
-    active_vars +=  "{\n";
-    active_vars +=  "\"function\": \"";
-    active_vars +=  func_->get_name();
-    active_vars +=  "\",\n";
+    active_vars += "{\n";
+    active_vars += "\"function\": \"";
+    active_vars += func_->get_name();
+    active_vars += "\",\n";
 
-    active_vars +=  "\"live_in\": {\n";
-    for (auto &p : live_in) {
-        if (p.second.size() == 0) {
+    active_vars += "\"live_in\": {\n";
+    for (auto &p : live_in)
+    {
+        if (p.second.size() == 0)
+        {
             continue;
-        } else {
-            active_vars +=  "  \"";
-            active_vars +=  p.first->get_name();
-            active_vars +=  "\": [" ;
-            for (auto &v : p.second) {
-                active_vars +=  "\"%";
-                active_vars +=  v->get_name();
-                active_vars +=  "\",";
+        }
+        else
+        {
+            active_vars += "  \"";
+            active_vars += p.first->get_name();
+            active_vars += "\": [";
+            for (auto &v : p.second)
+            {
+                active_vars += "\"%";
+                active_vars += v->get_name();
+                active_vars += "\",";
             }
-            active_vars += "]" ;
+            active_vars += "]";
             active_vars += ",\n";
         }
     }
     active_vars += "\n";
-    active_vars +=  "    },\n";
+    active_vars += "    },\n";
 
-    active_vars +=  "\"live_out\": {\n";
-    for (auto &p : live_out) {
-        if (p.second.size() == 0) {
+    active_vars += "\"live_out\": {\n";
+    for (auto &p : live_out)
+    {
+        if (p.second.size() == 0)
+        {
             continue;
-        } else {
-            active_vars +=  "  \"";
-            active_vars +=  p.first->get_name();
-            active_vars +=  "\": [" ;
-            for (auto &v : p.second) {
-                active_vars +=  "\"%";
-                active_vars +=  v->get_name();
-                active_vars +=  "\",";
+        }
+        else
+        {
+            active_vars += "  \"";
+            active_vars += p.first->get_name();
+            active_vars += "\": [";
+            for (auto &v : p.second)
+            {
+                active_vars += "\"%";
+                active_vars += v->get_name();
+                active_vars += "\",";
             }
             active_vars += "]";
             active_vars += ",\n";
